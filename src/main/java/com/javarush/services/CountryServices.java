2d8f1febea3312ce891b44386fad5bdcc474a385
Anastasiia Dosyn <anastasiia.dosyn@student.tuke.sk>
2024-09-26 12:21:18 +0200

    refactor: move non business logic to CityDataProcessing class

--- src/main/java/com/javarush/services/CountryServices.java
+++ src/main/java/com/javarush/services/CountryServices.java
@@ -3,19 +3,28 @@ package com.javarush.services;
 import com.javarush.domain.Country;
 import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.util.Collections;
 import java.util.List;
 
 public class CountryServices {
     private final SessionFactory sessionFactory;
+    private static final Logger LOGGER = LoggerFactory.getLogger(CountryServices.class);
 
     public CountryServices(SessionFactory sessionFactory) {
         this.sessionFactory = sessionFactory;
     }
 
     public List<Country> getAll() {
-        Query<Country> query = sessionFactory.getCurrentSession().createQuery("select c from Country c join fetch c.languages", Country.class);
-        return query.list();
+        try {
+            Query<Country> query = sessionFactory.getCurrentSession().createQuery("select c from Country c join fetch c.languages", Country.class);
+            return query.list();
+        } catch (Exception e) {
+            LOGGER.error("Error fetching countries", e);
+            return Collections.emptyList();
+        }
     }
 
     //cover by tests, loggers
