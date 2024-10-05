ed752cb86391641ccea0ac2380246d8a14d43e58
Anastasiia Dosyn <anastasiia.dosyn@student.tuke.sk>
2024-10-05 19:30:38 +0200

    feat: add services

--- src/main/java/com/javarush/services/CityService.java
+++ src/main/java/com/javarush/services/CityService.java
@@ -1,57 +1,92 @@
 package com.javarush.services;
 
 import com.javarush.domain.entity.City;
-import org.hibernate.SessionFactory;
-import org.hibernate.query.Query;
+import com.javarush.domain.exceptions.CityException;
+import com.javarush.repository.CityRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class CityService {
-    private final SessionFactory sessionFactory;
-    private final CountryService countryService;
-    private static final Logger LOGGER = LoggerFactory.getLogger(CountryService.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(CityService.class);
 
-    public CityService(SessionFactory sessionFactory, CountryService countryService) {
-        this.sessionFactory = sessionFactory;
-        this.countryService = countryService;
+    public City getById(Long id) {
+        try {
+            City city = new CityRepository().getById(id);
+            if (city == null) {
+                LOGGER.error("ERROR :: city with id {} not found", id);
+                throw new CityException("ERROR :: city with id " + id + " not found");
+            }
+            return city;
+        }catch (Exception e){
+            LOGGER.error("ERROR :: cannot get city with id {}", id);
+            throw new CityException("ERROR :: cannot get city with id " + id);
+        }
+    }
+
+    public City save(City entity) {
+        try {
+            City city = new CityRepository().save(entity);
+            if (city == null) {
+                LOGGER.error("ERROR :: no city with id {}", entity.getId());
+                throw new CityException("ERROR :: no city with id " + entity.getId());
+            }
+            return city;
+        }catch(Exception e) {
+            LOGGER.error("ERROR :: cannot save city with id {}", entity.getId());
+            throw new CityException("ERROR :: cannot save city with id " + entity.getId());
+        }
+    }
+
+    public void delete(Long id) {
+        try{
+            new CityRepository().delete(id);
+        }catch (Exception e){
+            LOGGER.error("ERROR :: cannot delete city with id: {}", id);
+            throw new CityException("ERROR :: cannot delete city with id: " + id);
+        }
+    }
+
+    public List<City> getAll() {
+        try {
+            List<City> cities = new CityRepository().getAll();
+            if (cities.isEmpty()) {
+                LOGGER.error("ERROR :: no cities found");
+                throw new CityException("ERROR :: no cities found");
+            }
+            return cities;
+        }catch (Exception e){
+            LOGGER.error("ERROR :: cannot get all cities");
+            throw new CityException("ERROR :: cannot get all cities");
+        }
     }
 
     public List<City> getItems(int offset, int limit) {
-        try {
-            System.out.println("HELLO from getItems");
-            Query<City> query = sessionFactory.getCurrentSession().createQuery("select c from City c", City.class);
-            query.setFirstResult(offset);
-            query.setMaxResults(limit);
-            return query.list();
-        } catch (Exception e) {
-            LOGGER.error("Error fetching cities with offset {} and limit {}. Error message :: {}", offset, limit, e.getMessage());
-            return new ArrayList<>();
+        try{
+            List<City> cities = new CityRepository().getItems(offset, limit);
+            if (cities.isEmpty()) {
+                LOGGER.error("ERROR :: no cities found in range");
+                throw new CityException("ERROR :: no cities found in range");
+            }
+            return cities;
+        }catch (Exception e){
+            LOGGER.error("ERROR :: cannot get all cities in range");
+            throw new CityException("ERROR :: cannot get all cities in range");
         }
     }
 
     public int getTotalCount() {
-        try {
-            System.out.println("HELLO from getTotal count");
-            Query<Long> query = sessionFactory.getCurrentSession().createQuery("select count(c) from City c", Long.class);
-            return Math.toIntExact(query.uniqueResult());
-        } catch (Exception e) {
-            LOGGER.error("Error fetching total city count. Error message :: {}", e.getMessage());
-            return 0;
-        }
-    }
-
-    public City getById(Integer id) {
-        try {
-            System.out.println("HELLO from get by id");
-            Query<City> query = sessionFactory.getCurrentSession().createQuery("select c from City c join fetch c.countryId where c.id = :ID", City.class);
-            query.setParameter("ID", id);
-            return query.getSingleResult();
-        } catch (Exception e) {
-            LOGGER.error("Error fetching city with ID {}. Error message :: {}", id, e.getMessage());
-            return null;
+        try{
+            int totalCount = new CityRepository().getTotalCount();
+            if (totalCount == 0) {
+                LOGGER.error("ERROR :: total cities count is 0");
+                throw new CityException("ERROR :: total cities count is 0");
+            }
+            return totalCount;
+        }catch(Exception e){
+            LOGGER.error("ERROR :: cannot get all cities count");
+            throw new CityException("ERROR :: cannot get all cities count");
         }
     }
 }
--- src/main/java/com/javarush/services/CountryService.java
+++ src/main/java/com/javarush/services/CountryService.java
@@ -1,29 +1,92 @@
 package com.javarush.services;
 
 import com.javarush.domain.entity.Country;
-import org.hibernate.SessionFactory;
-import org.hibernate.query.Query;
+import com.javarush.domain.exceptions.CountryException;
+import com.javarush.repository.CountryRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Collections;
 import java.util.List;
 
 public class CountryService {
-    private final SessionFactory sessionFactory;
     private static final Logger LOGGER = LoggerFactory.getLogger(CountryService.class);
 
-    public CountryService(SessionFactory sessionFactory) {
-        this.sessionFactory = sessionFactory;
+    public Country getById(Long id) {
+        try {
+            Country country = new CountryRepository().getById(id);
+            if (country == null) {
+                LOGGER.error("ERROR :: country with id {} not found", id);
+                throw new CountryException("ERROR :: country with id " + id + " not found");
+            }
+            return country;
+        }catch (Exception e){
+            LOGGER.error("ERROR :: cannot get country with id {}", id);
+            throw new CountryException("ERROR :: cannot get country with id " + id);
+        }
+    }
+
+    public Country save(Country entity) {
+        try {
+            Country country = new CountryRepository().save(entity);
+            if (country == null) {
+                LOGGER.error("ERROR :: no country with id {}", entity.getId());
+                throw new CountryException("ERROR :: no country with id " + entity.getId());
+            }
+            return country;
+        }catch(Exception e) {
+            LOGGER.error("ERROR :: cannot save country with id {}", entity.getId());
+            throw new CountryException("ERROR :: cannot save country with id " + entity.getId());
+        }
+    }
+
+    public void delete(Long id) {
+        try{
+            new CountryRepository().delete(id);
+        }catch (Exception e){
+            LOGGER.error("ERROR :: cannot delete country with id: {}", id);
+            throw new CountryException("ERROR :: cannot delete country with id: " + id);
+        }
     }
 
     public List<Country> getAll() {
         try {
-            Query<Country> query = sessionFactory.getCurrentSession().createQuery("select c from Country c join fetch c.languages", Country.class);
-            return query.list();
-        } catch (Exception e) {
-            LOGGER.error("Error fetching countries", e);
-            return Collections.emptyList();
+            List<Country> countries = new CountryRepository().getAll();
+            if (countries.isEmpty()) {
+                LOGGER.error("ERROR :: no countries found");
+                throw new CountryException("ERROR :: no countries found");
+            }
+            return countries;
+        }catch (Exception e){
+            LOGGER.error("ERROR :: cannot get all countries");
+            throw new CountryException("ERROR :: cannot get all countries");
+        }
+    }
+
+    public List<Country> getItems(int offset, int limit) {
+        try{
+            List<Country> countries = new CountryRepository().getItems(offset, limit);
+            if (countries.isEmpty()) {
+                LOGGER.error("ERROR :: no countries found in range");
+                throw new CountryException("ERROR :: no countries found in range");
+            }
+            return countries;
+        }catch (Exception e){
+            LOGGER.error("ERROR :: cannot get all countries in range");
+            throw new CountryException("ERROR :: cannot get all countries in range");
+        }
+    }
+
+    public int getTotalCount() {
+        try{
+            int totalCount = new CountryRepository().getTotalCount();
+            if (totalCount == 0) {
+                LOGGER.error("ERROR :: total countries count is 0");
+                throw new CountryException("ERROR :: total countries count is 0");
+            }
+            return totalCount;
+        }catch(Exception e){
+            LOGGER.error("ERROR :: cannot get all countries count");
+            throw new CountryException("ERROR :: cannot get all countries count");
         }
     }
 }
