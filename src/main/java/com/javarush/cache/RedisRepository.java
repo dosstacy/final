d32696d1dfad60ea859822a72d185ed665baff52
Anastasiia Dosyn <anastasiia.dosyn@student.tuke.sk>
2024-10-06 14:48:47 +0200

    fix: bugs fixed

--- src/main/java/com/javarush/cache/RedisRepository.java
+++ src/main/java/com/javarush/cache/RedisRepository.java
@@ -1,37 +1,74 @@
 package com.javarush.cache;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.javarush.cache.redis_enum.RedisModuleCommand;
-import redis.clients.jedis.Jedis;
+import com.javarush.services.CityService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import redis.clients.jedis.UnifiedJedis;
+import redis.clients.jedis.util.SafeEncoder;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class RedisRepository<T> {
-    private final Jedis jedis;
-    private final ObjectMapper objectMapper;
-    private static final String TOP_K_NAME = "topk_queries";
+public class RedisRepository {
+    private final UnifiedJedis jedis;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    private static final String TOPK_NAME = "city-country";
+    private static final int THRESHOLD = 7;
+    private static final Logger LOGGER = LoggerFactory.getLogger(CityService.class);
 
     public RedisRepository() {
-        this.objectMapper = new ObjectMapper();
-        this.jedis = new Jedis();
-        jedis.sendCommand(RedisModuleCommand.RESERVE, TOP_K_NAME, "500");
-    }
+        jedis = new UnifiedJedis("redis://127.0.0.1:6379");
 
-    public void save(String key, T entity) {
-        jedis.set(key, serialize(entity));
-    }
-
-    public T getById(String key, Class<T> clazz) { //?
-        String serializedEntity = jedis.get(key);
-        if (serializedEntity != null) {
-            jedis.sendCommand(RedisModuleCommand.ADD, TOP_K_NAME, key);
-            return deserialize(serializedEntity, clazz);
+        jedis.del(TOPK_NAME);
+        if (!jedis.exists(TOPK_NAME)) {
+            jedis.topkReserve(TOPK_NAME, 5L, 2000L, 7L, 0.925D);
         }
-        return null;
     }
 
-    public void delete(String key) {
+    public <T> T getById(String key, Class<T> clazz) {
+        String serializedEntity = jedis.get(key);
+        return deserialize(serializedEntity, clazz);
+    }
+
+    public <T> void put(String key, T value) {
+        jedis.topkAdd(TOPK_NAME, key);
+
+        if (getCount(key) >= THRESHOLD) {
+            System.out.println(serialize(value));
+            jedis.set(key, serialize(value));
+        }
+    }
+
+    private int getCount(String key) {
+        String count = jedis.sendCommand(() -> SafeEncoder.encode("TOPK.COUNT"), SafeEncoder.encodeMany(TOPK_NAME, key)).toString();
+        return Integer.parseInt(count.replaceAll("[\\[\\]]", ""));
+    }
+
+    public boolean checkExists(String key) {
+        return jedis.exists(key);
+    }
+
+    private <T> String serialize(T entity) {
+        try {
+            return objectMapper.writeValueAsString(entity);
+        } catch (Exception e) {
+            LOGGER.error("Error serializing object: " + e.getMessage());
+            return null;
+        }
+    }
+
+    private <T> T deserialize(String serializedEntity, Class<T> clazz) {
+        try {
+            return objectMapper.readValue(serializedEntity, clazz);
+        } catch (Exception e) {
+            LOGGER.error("Error deserializing object: " + e.getMessage());
+            return null;
+        }
+    }
+}
+
+/*public void save(String key, T entity) {
+       jedis.set(key, serialize(entity));
+
+public void delete(String key) {
         jedis.del(key);
     }
 
@@ -55,32 +92,4 @@ public class RedisRepository<T> {
 
     public int getTotalCount(String listName) {
         return Math.toIntExact(jedis.llen(listName));
-    }
-
-    public List<String> getTopKQueries(int k) {
-        Object object = jedis.sendCommand(RedisModuleCommand.LIST, TOP_K_NAME, String.valueOf(k));
-        if (object instanceof List) {
-            return (List<String>) object;
-        }
-        System.out.println("cannot cast object to list");
-        return null;
-    }
-
-    private String serialize(T entity) {
-        try {
-            return objectMapper.writeValueAsString(entity);
-        } catch (Exception e) {
-            System.err.println("Error serializing object: " + e.getMessage());
-            return null;
-        }
-    }
-
-    private T deserialize(String serializedEntity, Class<T> clazz) {
-        try {
-            return objectMapper.readValue(serializedEntity, clazz);
-        } catch (Exception e) {
-            System.err.println("Error deserializing object: " + e.getMessage());
-            return null;
-        }
-    }
-}
+    }*/
